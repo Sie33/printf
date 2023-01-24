c4271885e7fb6d0d7e112904829bd90761726716
sayv-xo <savegawugah01@gmail.com>
2023-01-24 12:28:12 +0000

    Add header file

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+/**
+ * struct spc - struct for format identifiers
+ *
+ * @spec: the format specifier
+ * @f: pointer to function
+*/
+
+typedef struct spc
+{
+	char *spec;
+	int (*f)(va_list fmt);
+} spec_f;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int fmt_spec(const char *format, va_list fmt);
+int c_spec(va_list fmt);
+int d_spec(va_list fmt);
+int i_spec(va_list fmt);
+int s_spec(va_list fmt);
+
+#endif
