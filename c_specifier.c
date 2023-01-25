28642a683187f9e37e5a7e6ffed141719082e4e8
sayv-xo <savegawugah01@gmail.com>
2023-01-25 03:59:27 +0000

    Added for c specifier

--- c_specifier.c
+++ c_specifier.c
@@ -5,10 +5,41 @@
 *
 * @fmt: variable list
 *
-* Return : return on success 1
+* Return: return on success 1
 */
 int c_spec(va_list fmt)
 {
 int s1 = va_arg(fmt, int);
 return (_putchar(s1));
+}
+
+/**
+ * s_spec - fetches a string from va_list
+ * @fmt: va_list array
+ *
+ * Return: an int equivalent to the chars of string on success
+ */
+int s_spec(va_list fmt)
+{
+	char *s = va_arg(fmt, char *);
+	int i, length;
+
+	i = 0;
+	length = 0;
+	if (s != NULL)
+	{
+		while (s[i])
+		{
+			length += _putchar(s[i]);
+			i++;
+		}
+		return (length);
+	}
+	length += _putchar('(');
+	length += _putchar('n');
+	length += _putchar('u');
+	length += _putchar('l');
+	length += _putchar('l');
+	length += _putchar(')');
+	return (length);
 }
